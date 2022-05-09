c7973b3c9b6447a1e86128d5a15d0bcc8c816206
Sadowski <krystek.sadowski@gmail.com>
2022-05-09 22:40:44 +0200

    [mod] get data of plans

--- commands/plans/getplan.js
+++ commands/plans/getplan.js
@@ -12,23 +12,27 @@ module.exports = {
     minArgs: 1,
 
     execute: async (client, message, args) =>{
+        let cos
         await mongo().then(async mongoose =>{
             try{
                         
                         const result = await cachedStudentsPlansSvgSchema.findOne({_id: args[0].toLocaleLowerCase()})
                         // console.log(result);
-                        if(result){
-                            htmltoimg(result['plan']).then((img)=>{
-                                message.channel.send({content: `To jest plan klasy ${args[0].toLocaleLowerCase()}\n⁣`, files: [{ attachment: img }]})
-                            })
-                        }else{
-                            message.channel.send('Nie znaleziono takiej klasy')
-                        }
+                        cos = result
+                        
                         
                         
             }finally{
                 mongoose.connection.close()
             }
         })
+        if(cos){
+            htmltoimg(cos['plan']).then((img)=>{
+                message.channel.send({content: `To jest plan klasy ${args[0].toLocaleLowerCase()}\n⁣`, files: [{ attachment: img }]})
+            })
+        }else{
+            message.channel.send('Nie znaleziono takiej klasy')
+        }
+        // console.log(cos);
     },
 };
