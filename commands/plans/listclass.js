f0f6ef97cf43f9071c04b07a999ff21b3cd2ec2d
krystek <krystek.sadowski@gmail.com>
2022-05-13 14:30:07 +0200

    [mod] listclass

--- commands/plans/listclass.js
+++ commands/plans/listclass.js
@@ -1,4 +1,5 @@
 const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 const cachedStudentsPlansSvgSchema = require('../../schemas/cached-students-plans-svg-schema.js');
 const classroomsSchema = require('../../schemas/classrooms-schema.js');
 const mongo = require("../../src/mongo");
@@ -16,12 +17,13 @@ module.exports = {
         let end = 10
         const nextButton = new MessageButton();
         const previosButton = new MessageButton();
+        // console.log(message);
         nextButton.setLabel('▶️');
         nextButton.setStyle('SECONDARY')
-        nextButton.setCustomId('mextClassroom');
+        nextButton.setCustomId('mextClassroom'+message.createdTimestamp);
         previosButton.setStyle('SECONDARY')
         previosButton.setLabel('◀️');
-        previosButton.setCustomId('previosClassroom');
+        previosButton.setCustomId('previosClassroom'+message.createdTimestamp);
         const row = new MessageActionRow().addComponents(previosButton, nextButton);
         const filter = (interaction) =>{
             if(interaction.user.id == message.author.id){
@@ -45,46 +47,57 @@ module.exports = {
             }finally{
                 mongoose.connection.close()
             }
-        })    
+        })
+            
         // console.log(saletable);
         module.exports.getdata(start, end, message, row, null, saletable) 
-        const collector = message.channel.createMessageComponentCollector({
+        // const collector = message.channel.createMessageComponentCollector({
+            // https://stackoverflow.com/questions/71098387/error-when-more-than-one-fight-in-same-channel
+            // const reply = await message.fetchReply();
+            const collector = await message.createMessageComponentCollector({ 
             filter,
             time: 10000
         });
-        collector.on('collect', interaction => {
-            // interaction.deferUpdate() 
-            if(interaction.customId  == 'mextClassroom'){
-                start  = start + 10
-                end = end +10
-                if(end<=saletable.length){
-                    module.exports.getdata(start, end, message, row, interaction.message, saletable)
-                    collector.resetTimer()
-                }else{
-                    end = saletable.length
-                    start  = saletable.length - 10
-                    module.exports.getdata(start, end, message, row, interaction.message, saletable)
-                    collector.resetTimer()
-                }
+        let i = 0
+        collector.on('collect', async interaction => {
+            collector.resetTimer()
+            i++
+            await interaction.deferUpdate();
+            await wait(4000);
+            await interaction.editReply({ content: `cos2 ${i}`, components: [row] });
+            // await interaction.update({ content: `cos2 ${i}`, components: [row] })
+            // // interaction.deferUpdate() 
+            // if(interaction.customId  == 'mextClassroom'){
+            //     start  = start + 10
+            //     end = end +10
+            //     if(end<=saletable.length){
+            //         module.exports.getdata(start, end, message, row, interaction.message, saletable)
+            //         collector.resetTimer()
+            //     }else{
+            //         end = saletable.length
+            //         start  = saletable.length - 10
+            //         module.exports.getdata(start, end, message, row, interaction.message, saletable)
+            //         collector.resetTimer()
+            //     }
                 
                 
                 
-            }else if(interaction.customId  == 'previosClassroom'){
-                start  = start - 10
-                end = end -10
-                if(start>=0){
-                    module.exports.getdata(start, end, message, row, interaction.message, saletable) 
-                    collector.resetTimer()
+            // }else if(interaction.customId  == 'previosClassroom'){
+            //     start  = start - 10
+            //     end = end -10
+            //     if(start>=0){
+            //         module.exports.getdata(start, end, message, row, interaction.message, saletable) 
+            //         collector.resetTimer()
                     
-                }else{
-                    start = 0
-                    end = 10
-                    module.exports.getdata(start, end, message, row, interaction.message, saletable)
-                    collector.resetTimer()
-                }
+            //     }else{
+            //         start = 0
+            //         end = 10
+            //         module.exports.getdata(start, end, message, row, interaction.message, saletable)
+            //         collector.resetTimer()
+            //     }
                  
-            }
-            // interaction.reply('Clicked!')
+            // }
+            // // interaction.reply('Clicked!')
             
           })
           collector.on('end', collected => {
@@ -104,7 +117,7 @@ module.exports = {
         embed.setDescription(str);
                 embed.setFooter({ text: `Showing ${end} of ${tablica.length}` });
                 if(edit == null){
-                    message.channel.send({ embeds: [embed], components: [row] });
+                    message.channel.send({ content: "cos", components: [row] });
                 }else{
                     edit.edit({ embeds: [embed], components: [row] })
                 }
